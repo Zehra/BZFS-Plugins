d05043e9860189afa3a95b9ab2b936e9e9159bfa
Zehra <Zehra@users.noreply.github.com>
2018-07-01 18:04:58 -0400

    added license to c++ file

--- Kamikaze.cpp
+++ Kamikaze.cpp
@@ -1,3 +1,15 @@
+/* Kamikaze.cpp
+* Copyright (c) 2018 Zehra
+*
+* This package is free software;  you can redistribute it and/or
+* modify it under the terms of the LGPL 2.1 license.
+* https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html
+*
+* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
 // Kamikaze.cpp : Defines the entry point for the DLL application.
 
 #include "bzfsAPI.h"
