31be9462cdc439e9d0d2c637f33a63397cd05d7e
Zehra <Zehra@users.noreply.github.com>
2018-07-03 20:06:06 -0400

    added point less self kills

---
+++ PointLessSelfKills.cpp
@@ -0,0 +1,51 @@
+/*
+ *  Copyright (C) 2018 Zehra
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option) any
+ * later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "bzfsAPI.h"
+
+class PointLessSelfKills : public bz_Plugin
+{
+public:
+  virtual const char* Name() { return "PointLessSelfKills"; }
+  virtual void Init(const char* /*config*/);
+  virtual void Event(bz_EventData *eventData);
+  virtual void Cleanup(void);
+};
+
+BZ_PLUGIN(PointLessSelfKills)
+
+void PointLessSelfKills::Init(const char*config)
+{
+  Register(bz_ePlayerDieEvent);
+}
+
+void PointLessSelfKills::Cleanup(void)
+{
+  Flush();
+}
+
+void PointLessSelfKills::Event(bz_EventData *eventData)
+{
+  if (eventData->eventType == bz_ePlayerDieEvent)
+  {
+    bz_PlayerDieEventData_V1 *dieData = (bz_PlayerDieEventData_V1*)eventData;
+    if (dieData->playerID == dieData->killerID)
+    { 
+      bz_incrementPlayerLosses(dieData->playerID, -1);
+    }
+  }
+}
