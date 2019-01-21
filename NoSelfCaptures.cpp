40ae131b7fca0b328c7e4c44a3ae7339f5c3d59e
Zehra <Zehra@users.noreply.github.com>
2019-01-21 17:44:18 -0500

    improved code

--- NoSelfCaptures.cpp
+++ NoSelfCaptures.cpp
@@ -21,37 +21,28 @@
 
 class NoSelfCaptures : public bz_Plugin
 {
+public:
   virtual const char* Name() { return "NoSelfCaptures"; }
   virtual void Init(const char* /*config*/);
   virtual void Event(bz_EventData *eventData);
   virtual void Cleanup(void);
-public:
 };
 
 BZ_PLUGIN(NoSelfCaptures)
 
-void NoSelfCaptures::Init(const char*config)
-{
+void NoSelfCaptures::Init(const char*config) {
   Register(bz_eAllowCTFCaptureEvent);
 }
 
-void NoSelfCaptures::Cleanup(void)
-{
+void NoSelfCaptures::Cleanup(void) {
   Flush();
 }
 
-void NoSelfCaptures::Event(bz_EventData *eventData)
-{
-  switch (eventData->eventType)
-  {
-    case bz_eAllowCTFCaptureEvent:
-    {
-      bz_AllowCTFCaptureEventData_V1 *capData = (bz_AllowCTFCaptureEventData_V1*)eventData;
-      if (bz_getPlayerTeam(capData->playerCapping) == capData->teamCapped)
-      {
-        capData->allow = false;
-      }
-      else capData->allow = true;
-    }break;
+void NoSelfCaptures::Event(bz_EventData *eventData) {
+  if (eventData->eventType == bz_eAllowCTFCaptureEvent) {
+    bz_AllowCTFCaptureEventData_V1 *capData = (bz_AllowCTFCaptureEventData_V1*)eventData;
+    if (bz_getPlayerTeam(capData->playerCapping) == capData->teamCapped) {
+      capData->allow = false;
+    }
   }
 }
